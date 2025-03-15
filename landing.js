e362b5fa04dc9454ce68759c2d46c8cbd99eebf1
victor.conceicao <victor.engenhariasoftware@gmail.com>
2025-03-15 21:46:45 +0000

    Mensagem descritiva do que foi alterado

--- landing.js
+++ landing.js
@@ -1,5 +1,7 @@
 document.getElementById('solicitarOrcamento').addEventListener('click', function() {
-    window.open('http://127.0.0.1:5500/formulario.html', '_blank'); // Abre em nova aba
+    window.open('https://luciadecor.netlify.app/formulario.html', '_blank'); // Usando caminho relativo
 });
+
+
 
 
