d24332d2561dbfd7a66ea786bab65263e0b52f81
victor.conceicao <victor.engenhariasoftware@gmail.com>
2025-03-15 08:55:51 +0000

    Primeiro commit da landing page

---
+++ landing.js
@@ -0,0 +1,5 @@
+document.getElementById('solicitarOrcamento').addEventListener('click', function() {
+    window.open('http://127.0.0.1:5500/formulario.html', '_blank'); // Abre em nova aba
+});
+
+
